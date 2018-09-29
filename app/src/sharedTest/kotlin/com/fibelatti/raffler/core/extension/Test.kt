920344dc262a8e8ec9fee339474a1eec08b9e72a
Filipe Belatti <fibelatti@gmail.com>
2018-09-29 23:55:48 +0100

    Adds method to mock suspend functions

--- app/src/sharedTest/kotlin/com/fibelatti/raffler/core/extension/Test.kt
+++ app/src/sharedTest/kotlin/com/fibelatti/raffler/core/extension/Test.kt
@@ -2,7 +2,10 @@
 
 package com.fibelatti.raffler.core.extension
 
+import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Assert.assertEquals
+import org.mockito.BDDMockito
+import org.mockito.BDDMockito.given
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
 
@@ -10,14 +13,15 @@ import org.mockito.Mockito.mock
  * Mockito.any() returns null and that can be an issue when testing Kotlin code.
  * This function addresses that issue and enables the usage of this matcher.
  */
-fun <T> any(): T {
-    Mockito.any<T>()
-    return uninitialized()
-}
+fun <T> safeAny(): T = Mockito.any<T>() ?: uninitialized()
 
 private fun <T> uninitialized(): T = null as T
 
 inline fun <reified T : Any> mock(): T = mock(T::class.java)
+
+fun <T> givenSuspend(methodCall: suspend () -> T): BDDMockito.BDDMyOngoingStubbing<T> {
+    return given(runBlocking { methodCall() })
+}
 
 // region Assertions
 infix fun Any.shouldBe(otherValue: Any) {
