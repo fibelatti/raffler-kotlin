cf9e2bccec397b68de7075c692daa0200aa61679
Filipe Belatti <fibelatti@gmail.com>
2018-10-06 10:08:30 +0100

    Fixes unit tests

--- app/src/sharedTest/kotlin/com/fibelatti/raffler/BaseTest.kt
+++ app/src/sharedTest/kotlin/com/fibelatti/raffler/BaseTest.kt
@@ -3,9 +3,12 @@ package com.fibelatti.raffler
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.fibelatti.raffler.core.provider.TestThreadProvider
 import com.fibelatti.raffler.core.provider.ThreadProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import org.junit.Rule
+import kotlin.coroutines.CoroutineContext
 
-abstract class BaseTest {
+abstract class BaseTest : CoroutineScope {
     @Suppress("LeakingThis")
     @get:Rule
     val injectMocks = InjectMocksRule.create(this)
@@ -14,4 +17,11 @@ abstract class BaseTest {
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     protected val testThreadProvider: ThreadProvider = TestThreadProvider()
+
+    override val coroutineContext: CoroutineContext
+        get() = testThreadProvider.main()
+
+    protected fun start(
+        block: suspend CoroutineScope.() -> Unit
+    ) = launch(testThreadProvider.main()) { block() }
 }
--- app/src/sharedTest/kotlin/com/fibelatti/raffler/core/extension/Test.kt
+++ app/src/sharedTest/kotlin/com/fibelatti/raffler/core/extension/Test.kt
@@ -9,24 +9,50 @@ import org.junit.Assert.assertTrue
 import org.mockito.BDDMockito
 import org.mockito.BDDMockito.given
 import org.mockito.Mockito
-import org.mockito.Mockito.mock
 import kotlin.reflect.KClass
 
 /***
  * Mockito.any() returns null and that can be an issue when testing Kotlin code.
  * This function addresses that issue and enables the usage of this matcher.
  */
-fun <T> safeAny(): T = Mockito.any<T>() ?: uninitialized()
+fun <T> safeAny(): T = Mockito.any<T>() ?: null as T
 
-private fun <T> uninitialized(): T = null as T
-
-inline fun <reified T : Any> mock(): T = mock(T::class.java)
-
-fun <T> givenSuspend(methodCall: suspend () -> T): BDDMockito.BDDMyOngoingStubbing<T> {
-    return given(runBlocking { methodCall() })
-}
+// region Coroutines
+/**
+ * This can be used when calling suspend functions in mocks, although since updating to Kotlin
+ * 1.3 it is not working properly with argument matchers.
+ * To test those scenarios make your Test Class inherit from [com.fibelatti.raffler.BaseTest]
+ * and call start { ... } placing your test body inside. InvalidUseOfMatchersException is still
+ * thrown but the tests will work.
+ *
+ * Example:
+ * ```
+ * @Test
+ * fun myTest() {
+ *     val mockedDependency: Dependency = mock()
+ *
+ *     val classToBeTested = ClassToBeTested(mockedDependency)
+ *
+ *     start {
+ *         // GIVEN
+ *         given(myMockedDependency.mySuspendFun())
+ *             .willReturn(myMockedResponse)
+ *
+ *         // WHEN
+ *         classToBeTested.methodToBeTested()
+ *
+ *         // THEN
+ *         // assertions
+ *     }
+ * }
+ *
+ * ```
+ */
+fun <T> givenSuspend(methodCall: suspend () -> T): BDDMockito.BDDMyOngoingStubbing<T> =
+    given(runBlocking { methodCall() })
 
 fun <T> callSuspend(methodCall: suspend () -> T): T = runBlocking { methodCall() }
+// endregion
 
 // region Assertions
 fun throwAssertionError() {
