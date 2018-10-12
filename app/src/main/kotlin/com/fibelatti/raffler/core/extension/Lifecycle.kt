8e1b3d2be3251ba4f5d222a6b13f0ea464fcfb8c
Filipe Belatti <fibelatti@gmail.com>
2018-10-12 20:13:39 +0100

    Adds event class

--- app/src/main/kotlin/com/fibelatti/raffler/core/extension/Lifecycle.kt
+++ app/src/main/kotlin/com/fibelatti/raffler/core/extension/Lifecycle.kt
@@ -3,9 +3,14 @@ package com.fibelatti.raffler.core.extension
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import com.fibelatti.raffler.core.platform.EventObserver
+import com.fibelatti.raffler.core.platform.Event
 
 fun <T : Any, L : LiveData<T>> LifecycleOwner.observe(liveData: L, body: (T) -> Unit) =
     liveData.observe(this, Observer { it?.let(body) })
+
+fun <T : Any, L : LiveData<Event<T>>> LifecycleOwner.observeEvent(liveData: L, body: (T) -> Unit) =
+    liveData.observe(this, EventObserver(body))
 
 fun <L : LiveData<Throwable>> LifecycleOwner.error(liveData: L, body: (Throwable) -> Unit) =
     liveData.observe(this, Observer { it?.let(body) })
