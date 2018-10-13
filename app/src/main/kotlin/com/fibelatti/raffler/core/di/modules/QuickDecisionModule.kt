cc0a04b92b37163848bd5624af5201afffdd2420
Filipe Belatti <fibelatti@gmail.com>
2018-10-13 19:49:23 +0100

    Implements add quick decision feature

--- app/src/main/kotlin/com/fibelatti/raffler/core/di/modules/QuickDecisionModule.kt
+++ app/src/main/kotlin/com/fibelatti/raffler/core/di/modules/QuickDecisionModule.kt
@@ -5,6 +5,7 @@ import com.fibelatti.raffler.core.di.modules.viewmodel.ViewModelKey
 import com.fibelatti.raffler.features.quickdecision.QuickDecisionRepository
 import com.fibelatti.raffler.features.quickdecision.data.QuickDecisionDataSource
 import com.fibelatti.raffler.features.quickdecision.presentation.QuickDecisionViewModel
+import com.fibelatti.raffler.features.quickdecision.presentation.addnew.AddNewQuickDecisionViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -20,4 +21,9 @@ abstract class QuickDecisionModule {
     @IntoMap
     @ViewModelKey(QuickDecisionViewModel::class)
     abstract fun bindQuickDecisionViewModel(quickDecisionViewModel: QuickDecisionViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(AddNewQuickDecisionViewModel::class)
+    abstract fun bindAddNewQuickDecisionViewModel(addNewQuickDecisionViewModel: AddNewQuickDecisionViewModel): ViewModel
 }
