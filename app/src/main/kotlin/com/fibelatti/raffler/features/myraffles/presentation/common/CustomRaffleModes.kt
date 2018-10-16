bfdc200942826ee4218cfb7a753da70bae653d4f
Filipe Belatti <fibelatti@gmail.com>
2018-10-16 20:15:55 +0100

    Updates voting icon and dialog behaviour

--- app/src/main/kotlin/com/fibelatti/raffler/features/myraffles/presentation/common/CustomRaffleModes.kt
+++ app/src/main/kotlin/com/fibelatti/raffler/features/myraffles/presentation/common/CustomRaffleModes.kt
@@ -3,8 +3,10 @@ package com.fibelatti.raffler.features.myraffles.presentation.common
 import android.content.Context
 import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
 import com.fibelatti.raffler.R
 import com.fibelatti.raffler.core.extension.visibleIf
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.fragment_custom_raffle_modes.*
 
@@ -36,6 +38,12 @@ class CustomRaffleModesDelegate : CustomRaffleModes {
             setupMode(groupingClickListener, groupGroupingViews, buttonGrouping)
             setupMode(combinationClickListener, groupCombinationViews, buttonCombination)
             setupMode(votingClickListener, groupVotingViews, buttonVoting)
+
+            setOnShowListener {
+                findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)?.let { bottomSheet ->
+                    BottomSheetBehavior.from(bottomSheet).state = BottomSheetBehavior.STATE_EXPANDED
+                }
+            }
 
             show()
         }
