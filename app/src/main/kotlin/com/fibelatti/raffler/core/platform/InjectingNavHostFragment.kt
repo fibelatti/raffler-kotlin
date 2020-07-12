2e2c235ced7e63b082ab6c8c0bfcf1fea2330c9f
Filipe Belatti <fibelatti@gmail.com>
2020-07-12 11:28:04 +0100

    Add missing Keep annotation

--- app/src/main/kotlin/com/fibelatti/raffler/core/platform/InjectingNavHostFragment.kt
+++ app/src/main/kotlin/com/fibelatti/raffler/core/platform/InjectingNavHostFragment.kt
@@ -1,9 +1,11 @@
 package com.fibelatti.raffler.core.platform
 
 import android.os.Bundle
+import androidx.annotation.Keep
 import androidx.navigation.fragment.NavHostFragment
 import com.fibelatti.raffler.core.platform.base.BaseActivity
 
+@Keep
 class InjectingNavHostFragment : NavHostFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
